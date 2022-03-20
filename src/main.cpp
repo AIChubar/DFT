f398009d986adc3136cd27d59a015f011a47c687
achubar.work <>
2022-03-20 22:18:21 +0100

    Creating

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL.h>
+
+//const int WIDTH = 800, HEIGHT = 600;
+
+int main(int arcg, char *argv[])
+{
+    std::cout << "sdl" << '\n';
+    std::cin.get();
+
+    return 0;
+}
